2fc8e1487eca0d57df6fafae5a1bf6ee59e8e62f
abesk <andrej.bauer@gmail.com>
2013-04-24 18:33:44 +0200

    coment deleted

--- my-maps-model/src/main/java/cz/muni/fi/pv243/mymaps/model/MapPermission.java
+++ my-maps-model/src/main/java/cz/muni/fi/pv243/mymaps/model/MapPermission.java
@@ -3,13 +3,6 @@ package cz.muni.fi.pv243.mymaps.model;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
-/**
- * Created with IntelliJ IDEA.
- * User: andrej
- * Date: 4/24/13
- * Time: 6:04 PM
- * To change this template use File | Settings | File Templates.
- */
 public class MapPermission {
 
     @NotNull
