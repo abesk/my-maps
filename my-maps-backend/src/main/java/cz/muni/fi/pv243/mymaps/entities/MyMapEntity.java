4e8a44186d1c4c321e693fe67cb64b79434ae584
Holmistr <>
2013-04-25 16:24:38 +0200

    Core of entities introduces

---
+++ my-maps-backend/src/main/java/cz/muni/fi/pv243/mymaps/entities/MyMapEntity.java
@@ -0,0 +1,87 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pv243.mymaps.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import org.hibernate.search.annotations.DateBridge;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.IndexedEmbedded;
+import org.hibernate.search.annotations.Resolution;
+
+/**
+ *
+ * @author Jiøí Holuša
+ */
+public class MyMapEntity extends AbstractEntity implements Serializable {
+    private static final long serialVersionUID = 19567189L;
+    
+    @Field
+    private String name;
+    
+    @Field
+    private ViewEntity view;
+    
+    @Field
+    @DateBridge(resolution= Resolution.MILLISECOND)
+    private Date creationDate;
+    
+    @Field
+    private UserEntity creator; 
+    
+    @IndexedEmbedded    
+    private List<PointOfInterestEntity> pointsOfInterest;
+
+    @Override
+    public String toString() {
+        return "cz.muni.fi.pv243.mymaps.entities.MyMapEntity[ id=" + id + " ]";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ViewEntity getView() {
+        return view;
+    }
+
+    public void setView(ViewEntity view) {
+        this.view = view;
+    }
+
+    public Date getCreationDate() {
+        return new Date(creationDate.getTime());
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = new Date(creationDate.getTime());
+    }
+
+    public UserEntity getCreator() {
+        return creator;
+    }
+
+    public void setCreator(UserEntity creator) {
+        this.creator = creator;
+    }
+
+    public List<PointOfInterestEntity> getPointsOfInterest() {
+        return Collections.unmodifiableList(pointsOfInterest);
+    }
+
+    public void setPointsOfInterest(Collection<PointOfInterestEntity> pointsOfInterest) {
+        this.pointsOfInterest = new ArrayList<>();
+        this.pointsOfInterest.addAll(pointsOfInterest);
+    }
+    
+}
--- my-maps-model/src/main/java/cz/muni/fi/pv243/mymaps/model/MyMap.java
+++ my-maps-model/src/main/java/cz/muni/fi/pv243/mymaps/model/MyMap.java
@@ -1,11 +1,14 @@
 package cz.muni.fi.pv243.mymaps.model;
 
+import java.util.ArrayList;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 
 public class MyMap {
@@ -16,7 +19,7 @@ public class MyMap {
     @Size(min = 1)
     private String name;
 
-    private Collection<PointOfInterest> pointsOfInterest;
+    private List<PointOfInterest> pointsOfInterest;
 
     @NotNull
     @Valid
@@ -45,12 +48,17 @@ public class MyMap {
         this.name = name;
     }
 
-    public Collection<PointOfInterest> getPointsOfInterest() {
-        return pointsOfInterest;
+    public List<PointOfInterest> getPointsOfInterest() {
+        if(pointsOfInterest == null){
+            return null;
+        }
+        
+        return Collections.unmodifiableList(pointsOfInterest);
     }
 
-    public void setPointsOfInterest(Collection<PointOfInterest> pointsOfInterest) {
-        this.pointsOfInterest = pointsOfInterest;
+    public void setPointsOfInterest(Collection<PointOfInterest> pointsOfInterest) {        
+        this.pointsOfInterest = new ArrayList<>();
+        this.pointsOfInterest.addAll(pointsOfInterest);
     }
 
     public View getView() {
@@ -62,11 +70,11 @@ public class MyMap {
     }
 
     public Date getCreationDate() {
-        return creationDate;
+        return new Date(creationDate.getTime());
     }
 
     public void setCreationDate(Date creationDate) {
-        this.creationDate = creationDate;
+        this.creationDate = new Date(creationDate.getTime());
     }
 
     public User getCreator() {
