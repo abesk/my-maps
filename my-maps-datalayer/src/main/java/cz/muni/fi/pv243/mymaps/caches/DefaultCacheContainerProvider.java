7089ad1b9b4d91a45ae8804ee879e81097056d83
Jimbulin <>
2013-05-19 01:05:24 +0200

    removed default templates comments

--- my-maps-datalayer/src/main/java/cz/muni/fi/pv243/mymaps/caches/DefaultCacheContainerProvider.java
+++ my-maps-datalayer/src/main/java/cz/muni/fi/pv243/mymaps/caches/DefaultCacheContainerProvider.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pv243.mymaps.caches;
 
 import java.io.IOException;
--- my-maps-datalayer/src/main/java/cz/muni/fi/pv243/mymaps/dao/MapPermissionDao.java
+++ my-maps-datalayer/src/main/java/cz/muni/fi/pv243/mymaps/dao/MapPermissionDao.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pv243.mymaps.dao;
 
 import cz.muni.fi.pv243.mymaps.entities.MapPermissionEntity;
